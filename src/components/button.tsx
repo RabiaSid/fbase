42810f54f7b2d5cb56d279c1042a5c44f7b43939
RabiaSid <rabiasid984@gmail.com>
2023-09-20 18:43:18 +0500

    set btn width

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ type propsType = {
     return (
       <button
         onClick={onClick}
-        className="rounded-full bg-teal-700 p-2 text-white px-8"
+        className="rounded-full bg-teal-700 p-2 text-white px-8 w-[30%]"
       >
         {label}
       </button>
